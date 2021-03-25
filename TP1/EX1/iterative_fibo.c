257bc617391c122a085435047d02011e7f23333f
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-03-25 14:39:05 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -7,13 +7,14 @@ int iterative_fibo(int n)
        return n;
    else
     {
-        int a,b;c;
+        int a,b,c;
         a=0;
         b=1;
-        for (int i=2,i<=n,i++)
+        for (int i=2;i<=n;i++)
             c=a+b;
             a=b;
             b=c;
         return c;
     }
+}
             
