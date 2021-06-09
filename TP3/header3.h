4cec275f7045865364de96f5963037263c2526d9
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-06-09 18:58:15 +0100

    Create header3.h

---
+++ TP3/header3.h
@@ -0,0 +1,13 @@
+#ifndef header2_h
+#define header2_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include"listeDouble.h"
+
+listeDouble  convert(char *);
+char palindrome(listeDouble);
+
+#endif
