175c2b456c7290210df84948a90f9792c0f17aee
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-06-12 13:48:51 +0100

    Update TP4.c

--- TP4/TP4.c
+++ TP4/TP4.c
@@ -1,5 +1,5 @@
 #include"TP4.h"
-
+//exercice 1
 Btree create_Btree(int x, Btree A,Btree B)
 {
     Btree C=(Btree)malloc(sizeof(Bnode));
@@ -53,3 +53,97 @@ int leaf_sum(Btree T)
 
 
 }
+//exercice 2
+
+void recursive_display_Inorder( Btree T)
+{
+    if(T==NULL)
+       return;
+    
+    recursive_display_Inorder(T->left);
+    printf("%d\n",T->data);
+    recursive_display_Inorder(T->right);
+    
+}
+void iterative_display_Inorder(Btree T)
+{
+    if (T==NULL)
+        return;
+    Bstack  A={NULL,0};
+    Btree tmp = T;
+    while(tmp!=NULL || A.lenght!=N){
+
+        if(tmp!=NULL){
+            push_Btree_stack(tmp , &A);
+            printf("%d    ",tmp->data);
+            tmp=tmp->left;
+        }
+        else{
+            tmp=pop_Btree_stack(&A);
+            printf("%d    ",tmp->data);
+            tmp=tmp->right;
+        }
+    }
+    return;
+    
+}
+
+
+
+
+
+void iterative_insert(Btree T, int x)
+{
+    Btree X =create_Btree(x,NULL,NULL);
+    if (T==NULL)
+        T=X ;
+    
+    /*if (Btree_depth(T)==0)
+    {
+        if (x > T->data)
+            T->right = X;
+        T->left = X ;
+    }*/
+    else
+    {
+        Btree tmp=T;
+        Btree tmp1=T;
+        while (tmp!=NULL)
+        {
+            tmp1=tmp;
+            if (x == tmp->data)
+                return;
+            if (x < tmp->data)
+                tmp = tmp->left;
+            else
+                tmp = tmp->right;
+        }
+        if (x == tmp->data)
+                return;
+        if (x < tmp->data)
+            tmp1->left=X;
+        else
+            tmp1->right=X;
+         
+    }
+
+
+}
+
+int recursive_searching(Btree T, int x)
+{
+    if(T==NULL)
+        return 0;
+    if(x==T->data)
+        return 1;
+    if(x<T->data)
+        return recursive_searching(T->left,x);
+    if(x>T->data)
+        return recursive_searching(T->right,x);
+    
+}
+int max_depth(Btree T){
+    return tree_depth(T);
+}
+
+
