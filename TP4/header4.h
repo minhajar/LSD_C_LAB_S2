408441f434cd930b6118dc8fd95f274946fb252b
Hajar LAMTAAI <72790103+minhajar@users.noreply.github.com>
2021-06-12 15:15:52 +0100

    Update header4.h

--- TP4/header4.h
+++ TP4/header4.h
@@ -14,13 +14,29 @@ struct Bnode
 typedef struct Bnode Bnode;
 typedef struct Bnode * Btree;
 
+#define N 100
+struct Btree_stack
+{
+    Btree top_stack[N];
+    unsigned lenght;
+} ;
+typedef struct Btree_stack Bstack;
+
 // prototypes//
 
 Btree create_Btree(int , Btree ,Btree );
+void push_Btree_stack(Btree , Bstack *);
+Btree pop_Btree_stack(Bstack *);
 int max(int ,int );
 int tree_depth(Btree );
 int tree_diameter(Btree );
 int leaf_sum(Btree );
+void iterative_insert(Btree , int );
+int recursive_searching(Btree , int );
+int max_depth(Btree );
+int tree_size(Btree);
+
+
 
 
 #endif
